24ea24b473aaca85e6049fd9dc6f7724c0bb0ec2
CJ Romero <cjromero51@yahoo.com>
2018-07-08 12:26:15 -0700

    player movement restrictions (still buggy)

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 // Enemies our player must avoid
-let allEnemies = [];
+
+
 var Enemy = function() {
   const yArray = [60, 145, 230];
   this.sprite = 'images/enemy-bug.png';
+  //inital coordinates for enemies
   this.x = -101;
   this.y = yArray[Math.floor(Math.random() * yArray.length)];
   allEnemies.push(this);
 
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// fixed speed for enemies; once off-canvas this.x & this.y are regenerated
 Enemy.prototype.update = function(dt) {
   if (this.x < 505) {
     this.x += 2;
@@ -20,48 +21,69 @@ Enemy.prototype.update = function(dt) {
     this.y = yArray[Math.floor(Math.random() * yArray.length)];
     this.x = -101;
   }
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 };
-// Draw the enemy on the screen, required method for game
+
 Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
 class Player {
   constructor() {
+    const movementArray = [];
     this.x = 202;
     this.y = 315;
-    this.charImage = 'images/char-boy.png'
-    this.update = function() {
-
-    };
+    this.charImage = 'images/char-boy.png';
     this.render = function() {
       ctx.drawImage(Resources.get(this.charImage), this.x, this.y);
     };
-    this.handleInput = function() {
+    this.handleInput = function(kC){
+      movementArray.push(kC);
     };
-  }
-}
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+    this.update = function() {
+      if (movementArray[0] == 37) {
+        while (movementArray.length > 0) {
+          movementArray.pop();
+        } if (this.x < 100) {
+          return this.x;
+        } else {
+          return this.x -= 101;
+      }};
+      if (movementArray[0] == 38) {
+        while (movementArray.length > 0) {
+          movementArray.pop();
+        } if (this.y < 100) {
+          return this.y = 315, this.x = 202;
+        } else {
+          return this.y -= 85;
+      }};
+      if (movementArray[0] == 39) {
+        while (movementArray.length > 0) {
+          movementArray.pop();
+        } if (this.x > 370) {
+          return this.x;
+        } else {
+        return this.x += 101;
+      }};
+      if (movementArray[0] == 40) {
+        while (movementArray.length > 0) {
+          movementArray.pop();
+        } if (this.y > 390) {
+          return this.y;
+        } else {
+          return this.y += 85;
+      }};
+    };
+  };
+};
+let allEnemies = [];
 let enemy = new Enemy();
 let enemyOne = setTimeout(function() {new Enemy()}, 1000);
 let enemyTwo = setTimeout(function() {new Enemy()}, 5000);
-let enemyFour = setTimeout(function() {new Enemy()}, 13000);
+let enemyFour = setTimeout(function() {new Enemy()}, 11000);
 let enemyFive = setTimeout(function() {new Enemy()}, 3000);
 let enemySix = setTimeout(function() {new Enemy()}, 7000);
-let enemyEight = setTimeout(function() {new Enemy()}, 15000);
 let player = new Player();
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -70,5 +92,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(e.keyCode);
 });
